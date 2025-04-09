79e6330e643b9aeb767c9b31c3a4d892f75b5514
aditya-raj2230 <aditya17025@gmail.com>
2025-04-10 03:24:50 +0530

    check

--- src/app/api/page/[id]/route.ts
+++ src/app/api/page/[id]/route.ts
@@ -2,27 +2,28 @@ import dbConnect from "@/lib/mongo";
 import { NextRequest, NextResponse } from "next/server";
 import Page from "@/model/Page";
 
-export async function GET(req:NextRequest,{params}:{params:{id:string}}) {
-    await dbConnect()
-    const page = await Page.findOne({id:params.id});
-    if(!page){
-        return NextResponse.json({text:""},{status:404})
-
+export async function GET(req: NextRequest, context: { params: { id: string } }) {
+    await dbConnect();
+    const { id } = context.params;
+    const page = await Page.findOne({ id });
+    if (!page) {
+        return NextResponse.json({ text: "" }, { status: 404 });
     }
-    return NextResponse.json({text:page.text})
+    return NextResponse.json({ text: page.text });
 }
 
-export async function POST(req: NextRequest, { params }: { params: { id: string } }) {
+export async function POST(req: NextRequest, context: { params: { id: string } }) {
     const { text } = await req.json();
     await dbConnect();
-  
-    const existing = await  Page.findOne({ id: params.id });
+
+    const { id } = context.params;
+    const existing = await Page.findOne({ id });
     if (existing) {
-      existing.text = text;
-      await existing.save();
+        existing.text = text;
+        await existing.save();
     } else {
-      await Page.create({ id: params.id, text });
+        await Page.create({ id, text });
     }
-  
+
     return NextResponse.json({ success: true });
-  }
+}
