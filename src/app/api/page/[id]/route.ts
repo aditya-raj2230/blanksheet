d0225be465c51c09a780cd6ca251d3ef4afcd529
aditya-raj2230 <aditya17025@gmail.com>
2025-04-10 03:32:00 +0530

    check

--- src/app/api/page/[id]/route.ts
+++ src/app/api/page/[id]/route.ts
@@ -2,9 +2,9 @@ import dbConnect from "@/lib/mongo";
 import { NextRequest, NextResponse } from "next/server";
 import Page from "@/model/Page";
 
-export async function GET(req: NextRequest, context: { params: { id: string } }) {
+export async function GET(req: NextRequest, { params }: { params: { id: string } }) {
     await dbConnect();
-    const { id } = context.params;
+    const { id } = params;
     const page = await Page.findOne({ id });
     if (!page) {
         return NextResponse.json({ text: "" }, { status: 404 });
@@ -12,11 +12,11 @@ export async function GET(req: NextRequest, context: { params: { id: string } })
     return NextResponse.json({ text: page.text });
 }
 
-export async function POST(req: NextRequest, context: { params: { id: string } }) {
+export async function POST(req: NextRequest, { params }: { params: { id: string } }) {
     const { text } = await req.json();
     await dbConnect();
 
-    const { id } = context.params;
+    const { id } = params;
     const existing = await Page.findOne({ id });
     if (existing) {
         existing.text = text;
